a97657b4064615a075c930260c2d5edc5511163e
secondbay <haqisibay@gmail.com>
2026-04-10 16:58:41 +0700

    setup local build

---
+++ build/precache-manifest.2f7e3edab5e5ccac9d0a153a28684573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73157ce3a4a5ca46a267e3a35ff524df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a291a36ee4acccadec7",
+    "url": "./static/css/2.01bf8f10.chunk.css"
+  },
+  {
+    "revision": "7bf1f8e0c95742162608",
+    "url": "./static/css/main.719cc264.chunk.css"
+  },
+  {
+    "revision": "4a291a36ee4acccadec7",
+    "url": "./static/js/2.0ba0ba62.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.0ba0ba62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf1f8e0c95742162608",
+    "url": "./static/js/main.36688ddb.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
